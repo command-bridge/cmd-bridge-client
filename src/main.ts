7987133d52da48c53c73a93fbaa02e15e4257a12
Leandro Perrotta <leandro_perrotta@hotmail.com>
2024-11-10 19:57:55 -0300

    chore(package): adjusting to match build path

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
-require('electron-reload')(path.join(__dirname, '..', 'dist'), {
-  electron: path.join(__dirname, '..', 'node_modules', '.bin', 'electron')
-});
+
+const devMode = process.env.NODE_ENV === 'development';
+
+if (devMode) {
+
+    // supports hot-reload for electron when using ts-node
+    require('electron-reload')(path.join(__dirname, '..', 'dist'), {
+        electron: path.join(__dirname, '../..', 'node_modules', '.bin', 'electron')
+    });
+}
+
 
 import { loadControllers } from './process/modules/controllers';
 import { APP_NAME } from '../configs/consts';
@@ -10,44 +18,45 @@ import { APP_NAME } from '../configs/consts';
 let mainWindow: BrowserWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    title: APP_NAME,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false
-    }
-  });
 
-  const devMode = process.env.NODE_ENV === 'development';
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        title: APP_NAME,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
 
-  if (devMode) {
-    mainWindow.loadURL('http://localhost:8080');
-  } else {
-    mainWindow.loadFile(path.join(__dirname, '../dist/index.html'));
-  }
+    if (devMode) {
 
-  mainWindow.on('closed', () => {
-    mainWindow = null!;
-  });
+        mainWindow.loadURL('http://localhost:8080');
+        mainWindow.webContents.openDevTools();
+    } else {
+        mainWindow.loadFile(path.join(__dirname, '../dist/index.html'));
+    }    
+
+    mainWindow.on('closed', () => {
+        mainWindow = null!;
+    });
 }
 
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow();
-  }
+    if (mainWindow === null) {
+        createWindow();
+    }
 });
 
 app.whenReady().then(() => {
 
-  loadControllers();
+    loadControllers();
 });
